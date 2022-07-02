611421f5894493eb2d77ebd12954b3f5fbcb24af
Sachin Mishra <sachin19566@gmail.com>
2022-07-02 19:05:09 +0530

    Updated now

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import sys
+from housing.logger import logging
 app=Flask(__name__)
 
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    logging.info("We are testing logging module")
     return "CI/CD Pipeline Established."
 
 
